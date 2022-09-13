b78a5123179dd12bbc143496daa76c67212d2204
astonetuhame <astonetuhame@gmail.com>
2022-09-13 13:52:14 +0300

    Update settings.py

--- backend/src/database/settings.py
+++ backend/src/database/settings.py
@@ -2,4 +2,4 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 DB_FILENAME = os.environ.get("DB_FILENAME")
-PROJECT_DIR = os.environ.dirnameget(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
